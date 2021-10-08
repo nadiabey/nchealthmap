c123b9372a6ab52d3d825cbd6cd8cdff31f60ce3
AyaLahlou <50680354+AyaLahlou@users.noreply.github.com>
2021-10-07 22:54:27 -0400

    Update covid_death_race.sql

--- covid_death_race.sql
+++ covid_death_race.sql
@@ -7,3 +7,8 @@ CREATE TABLE covid_death_race
  native INTEGER NOT NULL,
  other INTEGER NOT NULL,
 );
+
+
+INSERT INTO covid_death_race VALUES(1, 300, 150, 100, 30, 10, 2, 8);
+INSERT INTO covid_death_race VALUES(2, 123, 50,  40,  10, 10, 4, 9 );
+INSERT INTO covid_death_race VALUES(3, 300, 120, 80, 50, 15, 15, 20 );
