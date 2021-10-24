41e0b45e31e946655bd3f53573d017220fdf79be
Nadia Bey <45720698+nadiabey@users.noreply.github.com>
2021-10-24 18:28:03 -0400

    Update facilities.sql

--- facilities.sql
+++ facilities.sql
@@ -11,6 +11,7 @@ INSERT INTO facility_type VALUES('ED', 'emergency department');
 INSERT INTO facility_type VALUES('RH', 'rehabilitation');
 INSERT INTO facility_type VALUES('VA', 'Veterans Affairs');
 INSERT INTO facility_type VALUES('UC', 'urgent care')
+INSERT INTO faclity_type VALUES('LTC', 'long term care')
 
 CREATE TABLE health_facilities(
 facility_id INTEGER PRIMARY KEY NOT NULL,
@@ -81,3 +82,115 @@ INSERT INTO health_facilities VALUES(29, 'Atrium Health Kings Mountain', 'HL',
 23, '35.2227371', '-81.3504593');
 INSERT INTO health_facilities VALUES(30, 'Columbus Regional Healthcare System',
 'HL', 24, '35.7736477', '-86.7880641');
+INSERT INTO health_facilities VALUES(31, 'CarolinaEast Medical Center',
+'HL', 25, '35.5486184', '-79.1204588');
+INSERT INTO health_facilities VALUES(32, 'Cape Fear Valley Medical Center',
+'HL', 26, '35.0931032', '-79.1296138');
+INSERT INTO health_facilities VALUES(33, 'Highsmith-Rainey Specialty Hospital',
+'LTC', 26, '35.0931032', '-79.1296138');
+INSERT INTO health_facilities VALUES(34, 'Womack Army Medical Center',
+'HL', 26, '35.14615', '-79.0042814');
+INSERT INTO health_facilities VALUES(35, 'Fayetteville VA Medical Center',
+'VA', 26, '35.0883197', '-78.8789595');
+INSERT INTO health_facilities VALUES(36, 'The Outer Banks Hospital', 'HL', 28,
+'35.9398093', '-75.6203057');
+INSERT INTO health_facilities VALUES(37, 'Lexington Memorial Hospital', 'HL',
+29, '35.8055777', '-80.2891995');
+INSERT INTO health_facilities VALUES(38, 'Thomasville Medical Center', 'HL', 29,
+'35.8807458', '-80.112843');
+INSERT INTO health_facilities VALUES(39, 'Davie Medical Center', 'HL', 30,
+  '36.0094689', '-80.4500083');
+INSERT INTO health_facilities VALUES(40, 'Davie Medical Center Bermuda Run',
+  'ED', 30, '36.0094586', '-80.4499901');
+INSERT INTO health_facilities VALUES(41, 'Vidant Duplin Hospital', 'HL', 31,
+'34.9651306', '-77.9641861');
+INSERT INTO health_facilities VALUES(42, 'North Carolina Specialty Hospital',
+'HL', 32, '36.0429083', '-78.8990836');
+INSERT INTO health_facilities VALUES(43, 'Duke University Hospital', 'HL',
+32, '36.0075095', '-78.9406307');
+INSERT INTO health_facilities VALUES(44, 'Duke Regional Hospital', 'HL',
+32, '36.0367592', '-78.9023223');
+INSERT INTO health_facilities VALUES(45, 'Select Specialty Hospital Durham',
+  'LTC', 32, '36.0367592', '-78.9023223');
+INSERT INTO health_facilities VALUES(46, 'Durham VA Medical Center', 'VA',
+32, '36.0090165', '-78.9408415');
+INSERT INTO health_facilities VALUES(47, 'Vidant Edgecombe Hospital', 'HL',
+33, '35.9212308', '-77.5578614');
+INSERT INTO health_facilities VALUES(48, 'Novant Medical Park Hospital', 'HL',
+  34, '36.0788971', '-80.2983839');
+INSERT INTO health_facilities VALUES(49, 'Wake Forest Baptist Medical Center',
+'HL', 34, '36.0895742', '-80.2700346');
+INSERT INTO health_facilities VALUES(50, 'Novant Health Forsyth Medical Center',
+'HL', 34, '36.0765746', '-80.2984536');
+INSERT INTO health_facilities VALUES(51, 'Novant Health Kernersville Medical Center',
+'HL', 34, '36.0912608', '-80.046167');
+INSERT INTO health_facilities VALUES(52, 'Novant Health Clemmons Medical Center',
+'HL', 34, '36.0283742', '-80.4000158');
+INSERT INTO health_facilities VALUES(53, 'Old Vineyard Behavioral Health Services',
+'PSY', 34, '36.0783613', '-80.3207574');
+INSERT INTO health_facilities VALUES(54, 'CaroMont Regional Medical Center', 'HL',
+36, '35.2727598', '-81.1424316');
+INSERT INTO health_facilities VALUES(55, 'Granville Medical Center', 'HL',
+39, '36.2328244', '-78.7947784');
+INSERT INTO health_facilities VALUES(56, 'Central Regional Hospital', 'PSY',
+39, '36.1461317', '-78.7739896');
+INSERT INTO health_facilities VALUES(57, 'Moses H. Cone Memorial Hospital',
+'HL', 41, '36.0910505', '-79.7883839');
+INSERT INTO health_facilities VALUES(58, 'High Point Medical Center', 'HL', 41,
+'35.9621627', '-80.0154337');
+INSERT INTO health_facilities VALUES(59, 'Kindred Hospital Greensboro',
+   'LTC', 41, '36.0491809', '-79.7723778');
+INSERT INTO health_facilities VALUES(60, 'Cone Health MedCenter High Point',
+'ED', 41, '36.0438158', '-79.967158');
+INSERT INTO health_facilities VALUES(61,
+   'Cone Health Womens and Childrens Center at Moses Cone Hospital', 'HL',
+   41, '36.0910478', '-79.788388');
+INSERT INTO health_facilities VALUES(62, 'Select Specialty Hospital Greensboro',
+'LTC', 41, '36.0910489', '-79.7886888');
+INSERT INTO health_facilities VALUES(63, 'Fellowship Hall', 'PSY', 41,
+  '36.147882', '-79.7308933');
+INSERT INTO health_facilities VALUES(64, 'Vidant North Hospital', 'HL', 42,
+   '36.4322509', '-77.6484631');
+INSERT INTO health_facilities VALUES(65, 'Betsy Johnson Hospital', 'HL', 43,
+   '35.3071399', '-78.6370054');
+INSERT INTO health_facilities VALUES(66, 'Central Harnett Hospital', 'HL', 43,
+'35.4252815', '-78.8105839');
+INSERT INTO health_facilities VALUES(67, 'Good Hope Hospital', 'PSY', 43,
+'35.3276082', '-78.6719349');
+INSERT INTO health_facilities VALUES(68, 'Haywood Regional Medical Center',
+  'HL', 44, '35.5255083', '-82.9449502');
+INSERT INTO health_facilities VALUES(69, 'Pardee UNC Health Care', 'HL', 45,
+'35.3207792', '-82.4694031');
+INSERT INTO health_facilities VALUES(70, 'AdventHealth Hendersonville', 'HL',
+45, '35.3896801', '-82.4968877');
+INSERT INTO health_facilities VALUES(71, 'Vidant Roanoke Chowan Hospital', 'HL',
+46, '36.2855382', '-76.9970707');
+INSERT INTO health_facilities VALUES(72, 'Hoke Hospital', 'ED', 47, '35.029881',
+'-79.1094796');
+INSERT INTO health_facilities VALUES(73,
+  'FirstHealth Moore Regional Hospital Hoke Campus', 'ED', 47, '35.0209217',
+'-79.151707');
+INSERT INTO health_facilities VALUES(74, 'Davis Regional Medical Center',
+'HL', 49, '35.8186523', '-80.8313783');
+INSERT INTO health_facilities VALUES(75, 'Iredell Memorial Hospital', 'HL',
+49, '35.799738', '-80.8808091');
+INSERT INTO health_facilities VALUES(76, 'Lake Norman Regional Medical Center',
+'HL', 49, '35.5492724', '-80.8563502');
+INSERT INTO health_facilities VALUES(77, 'Harris Regional Hospital', 'HL',
+50, '35.3859427', '-83.2046477');
+INSERT INTO health_facilities VALUES(78, 'Johnston Health', 'HL', 51,
+'35.5141581', '-78.3361592');
+INSERT INTO health_facilities VALUES(79, 'Johnston Health Clayton', 51,
+'35.6311617', '-78.5055958');
+INSERT INTO health_facilities VALUES(80, 'Central Carolina Hospital', 53,
+'35.4688921', '-79.1893523');
+INSERT INTO health_facilities VALUES(81, 'Lenoir Memorial Hospital', 'HL',
+54, '35.2903724', '-77.5842354');
+INSERT INTO health_facilities VALUES(82, 'Atrium Health Lincoln', 'HL', 55,
+'35.7314867', '-80.6262483');
+INSERT INTO health_facilities VALUES(83, 'Angel Medical Center', 'HL', 56,
+'35.1844714', '-83.3816017');
+INSERT INTO health_facilities VALUES(84, 'Highlands-Cashiers Hospital', 'HL',
+56, '35.086187', '-83.1876555');
+INSERT INTO health_facilities VALUES(85, 'Martin General Hospital', 'HL', 58,
+'35.8397503', '-77.075287');
