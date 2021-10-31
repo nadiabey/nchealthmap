0d2eb750fbdf5e332897a45f9c4f40faa9846620
juliasulstarova <87027062+juliasulstarova@users.noreply.github.com>
2021-10-31 12:45:20 -0400

    Update briths_prenatalcare.sql

--- briths_prenatalcare.sql
+++ briths_prenatalcare.sql
@@ -3,8 +3,7 @@ CREATE TABLE births_prenatalcare(
   first_trimester FLOAT NOT NULL, -- % of people receiving prenatal care since their first trimester
   second_trimester FLOAT NOT NULL,
   third_trimester FLOAT NOT NULL,
-  none_prenatalcare FLOAT NOT NULL,
-  total FLOAT NOT NULL
+  none_prenatalcare FLOAT NOT NULL
 );
 
 INSERT INTO births_prenatalcare VALUES (1,32.9, 23.2, 11.5, 45);
