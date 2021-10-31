7ca04ac3137b7849ef10614a5cbeb903fff18e66
juliasulstarova <87027062+juliasulstarova@users.noreply.github.com>
2021-10-31 12:30:00 -0400

    Update education.sql

--- education.sql
+++ education.sql
@@ -1,6 +1,6 @@
 CREATE TABLE education(
   county_id INTEGER NOT NULL PRIMARY KEY REFERENCES county(id),
-  adults_no INTEGER NOT NULL FOREIGN KEY REFERENCES population(population_adults),
+  adults_no INTEGER NOT NULL, --% FOREIGN KEY REFERENCES population(population_adults),
   college_education FLOAT NOT NULL, --% of adults receiving higher education
   highschool_education FLOAT NOT NULL.
 );
