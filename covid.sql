118e396735a153dc8bd2c2d38fd47f739bf71a19
AyaLahlou <50680354+AyaLahlou@users.noreply.github.com>
2021-10-31 12:59:36 -0400

    Update covid.sql

--- covid.sql
+++ covid.sql
@@ -2,7 +2,6 @@ CREATE TABLE covid(
   county_id INTEGER NOT NULL PRIMARY KEY REFERENCES county(id),
   cases INTEGER NOT NULL,
   deaths INTEGER NOT NULL, 
-  hospitalizations INTEGER NOT NULL,
   vaccinated_at_least_1 INTEGER NOT NULL,
   vaccinated_fully INTEGER NOT NULL,
 );
