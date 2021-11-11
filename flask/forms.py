af1a29165a8566d25a5d869d0b02680267bbd9a3
Nadia Bey <45720698+nadiabey@users.noreply.github.com>
2021-11-11 03:13:12 -0500

    Update forms.py

--- flask/forms.py
+++ flask/forms.py
@@ -1,51 +1,50 @@
+import math
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, IntegerField, SelectField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, IntegerField, DecimalField, SelectField, SubmitField
+from wtforms.validators import DataRequired, Email, length, InputRequired
 
 
-class LocationForm(FlaskForm):
+class Filter(FlaskForm):
     location_type = SelectField('Type', choices=[], validators=[DataRequired()], id='lt')
     county = SelectField('County', choices=[], id='cty')
-    zip = IntegerField('Zip Code', id='zip')
-    submit = SubmitField('Update')
+    zip = IntegerField('Zip Code', validators=[], id='zip')
+    city = SelectField('City', choices=[], id='mun')
+    submit = SubmitField('Filter')
 
-""""
-class DrinkerEditFormFactory:
-    @staticmethod
-    def form(drinker, beers, bars):
-        class F(FlaskForm):
-            name = StringField(default=drinker.name)
-            address = StringField(default=drinker.address)
-            @staticmethod
-            def beer_field_name(index):
-                return 'beer_{}'.format(index)
-            def beer_fields(self):
-                for i, beer in enumerate(beers):
-                    yield beer.name, getattr(self, F.beer_field_name(i))
-            def get_beers_liked(self):
-                for beer, field in self.beer_fields():
-                    if field.data:
-                        yield beer
-            @staticmethod
-            def bar_field_name(index):
-                return 'bar_{}'.format(index)
-            def bar_fields(self):
-                for i, bar in enumerate(bars):
-                    yield bar.name, getattr(self, F.bar_field_name(i))
-            def get_bars_frequented(self):
-                for bar, field in self.bar_fields():
-                    if field.data != 0:
-                        yield bar, field.data
-        beers_liked = [like.beer for like in drinker.likes]
-        for i, beer in enumerate(beers):
-            field_name = F.beer_field_name(i)
-            default = 'checked' if beer.name in beers_liked else None
-            setattr(F, field_name, BooleanField(default=default))
-        bars_frequented = {frequent.bar: frequent.times_a_week\
-                           for frequent in drinker.frequents}
-        for i, bar in enumerate(bars):
-            field_name = F.bar_field_name(i)
-            default = bars_frequented[bar.name] if bar.name in bars_frequented else 0
-            setattr(F, field_name, IntegerField(default=default))
-        return F()
-"""
+
+class FeedbackForm(FlaskForm):
+    name = StringField(label='Name', validators=[length(max=256)])
+    email = StringField(validators=[Email(), length(max=256)])
+    comment = StringField(validators=[InputRequired(), length(max=1000)])
+    submit = SubmitField('Send')
+
+
+class Distance(FlaskForm):
+    latitude = DecimalField('Latitude')
+    longitude = DecimalField('Longitude')
+    nearest = SelectField('Facility Type', choices=[])  # query health facilities
+
+    def to_rad(self, deg: float) -> float:
+        return deg * math.pi / 180
+
+    def great_circle_dist(self, lat1, long1, lat2, long2):
+        return 2 * math.asin(math.sqrt(
+            (math.sin((lat1 - lat2) / 2)) ** 2 + math.cos(lat1) * math.cos(lat1) * math.sin((long1 - long2) / 2) ** 2))
+
+    def to_mi(self, dist: float) -> float:
+        if dist < 0:
+            dist += math.pi
+        dist *= 6371.2  # radius of earth in km
+        dist /= 1.609  # km to mi
+        return dist
+
+    def calculate_distance(self, fac: list):
+        """lat, long and fac will include form data
+        fac = list of coords for facilities with id"""
+        dist = [(x[0],
+                 self.to_mi(
+                     self.great_circle_dist(self.to_rad(self.latitude.data), self.to_rad(self.longitude.data),
+                                            self.to_rad(x[1]), self.to_rad(x[2]))))
+                for x in fac]
+        min_dist = min(dist, key=lambda x: x[1])
+        return min_dist
