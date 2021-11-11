84dbf1a1622d4b80abdd8933f404ca2925b735c0
Nadia Bey <45720698+nadiabey@users.noreply.github.com>
2021-11-11 03:12:42 -0500

    Update app.py

--- flask/app.py
+++ flask/app.py
@@ -1,71 +1,67 @@
 from flask import Flask, render_template, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
+from wtforms.validators import AnyOf
+
 import models, forms
 
 app = Flask(__name__)
-app.secret_key = 's3cr3t'
 app.config.from_object('config')
 db = SQLAlchemy(app, session_options={'autocommit': False})
 
 
 @app.route('/', methods=['GET', 'POST'])
 def choose_search():
-    form = forms.LocationForm()
-    lt = models.LocationType
-    form.location_type.choices = [(obj.short, obj.type) for obj in lt.query.all()]
-    if request.method == 'GET':
-        if request.form['Update'] == 'State':
-            return redirect(url_for('state'))
-        if request.form['Update'] == 'City':
-            return redirect(url_for('state/<city>'))
-        if request.form['Update'] == 'Zip':
-            return redirect(url_for('zip/<zip>'))
-        if request.form['Update'] == 'County':
-            return redirect(url_for('county/<cty>'))
+    form = forms.Filter()
+    form.location_type.choices = [(obj.short, obj.type) for obj in models.LocationType.query.all()]
+    form.county.choices = [(obj.id, obj.county) for obj in models.County.query.all()]
+    form.city.choices = [(obj.county_id, obj.city) for obj in models.City.query.all()]
+    if request.method == 'POST':    # post because input needs to be sent to sql
+        if form.location_type.data == 'cty':
+            return redirect(url_for('county', cid=form.county.data))
+        if form.location_type.data == 'mun':
+            return redirect(url_for('city', cty=form.city.data))
+        if form.location_type.data == 'zip':
+            return redirect(url_for('zip', zc=form.zip.data))
     return render_template('landing.html', form=form)
 
 
 @app.route('/state', methods=['GET', 'POST'])
-def location(lt):
-    return None
+def state():
+    return render_template('state.html')
+
+
+@app.route('/county/<cid>', methods=['GET', 'POST'])
+def county(cid):
+    obj = db.session.query(models.County).filter(models.County.id == cid).one()
+    return render_template('counties.html', county=obj)
+
+
+@app.route('/city/<cty>')
+def city(cty):
+    # make city template, link to county
+    return render_template('counties.html', county=cty)
+
+
+@app.route('/zip/<zc>')
+def zipcode(zc):
+    # make zips template, return county
+    return render_template('landing.html')
 
 
 @app.route('/feedback', methods=['GET', 'POST'])
 def feedback():
-    return render_template('')  # template for comment box
+    form = forms.FeedbackForm()
+    if request.method == 'POST' and form.validate():
+        info = models.Comment(name=form.name.data, email=form.email.data, comment=form.comment.data)
+        db.session.add(info)
+        db.session.commit()
+        return redirect(url_for('feedbacksent'))
+    return render_template('comment.html', form=form)
 
-"""
-@app.route('/drinker/<name>')
-def drinker(name):
-    drinker = db.session.query(models.Drinker)\
-        .filter(models.Drinker.name == name).one()
-    return render_template('drinker.html', drinker=drinker)
 
-@app.route('/edit-drinker/<name>', methods=['GET', 'POST'])
-def edit_drinker(name):
-    drinker = db.session.query(models.Drinker)\
-        .filter(models.Drinker.name == name).one()
-    beers = db.session.query(models.Beer).all()
-    bars = db.session.query(models.Bar).all()
-    form = forms.DrinkerEditFormFactory.form(drinker, beers, bars)
-    if form.validate_on_submit():
-        try:
-            form.errors.pop('database', None)
-            models.Drinker.edit(name, form.name.data, form.address.data,
-                                form.get_beers_liked(), form.get_bars_frequented())
-            return redirect(url_for('drinker', name=form.name.data))
-        except BaseException as e:
-            form.errors['database'] = str(e)
-            return render_template('edit-drinker.html', drinker=drinker, form=form)
-    else:
-        return render_template('edit-drinker.html', drinker=drinker, form=form)
-        
-"""
-
-@app.template_filter('pluralize')
-def pluralize(number, singular='', plural='s'):
-    return singular if number in (0, 1) else plural
-
+@app.route('/feedbacksent')
+def success():
+    return render_template('done.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
