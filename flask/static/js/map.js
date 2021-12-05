b7e02458cd0f67e8cab489d3f9321e00999a1205
Aryan Poonacha <poonacharyan@gmail.com>
2021-12-05 18:00:27 -0500

    color maps

--- flask/static/js/map.js
+++ flask/static/js/map.js
@@ -1,69 +1,66 @@
-var svg = d3.select("svg");
 
-//create projection for map and pass that to the path generator
-var projection = d3
-    .geoAlbers()
-    .rotate([0, 62, 0]);
+        var svg = d3.select("svg");
+        width = +svg.attr('width');
+        height = +svg.attr('height');
 
-var path = d3.geoPath(projection);
-var file = Flask.url_for("static", {"filename": "data/ncmap.json"});
-
-//to make sure the file is being passed
-console.log(file)
-$.getJSON(file, 
-    function (data) {
-    console.log(data)
-    })
-
-d3.json(file, function (error, mapData) {
-    if (error) throw error;
-
-    var projection = d3
-        .geoAlbers()
-        .rotate([0, 62, 0])
-        .fitSize([d3.select("svg").attr("width")-50, d3.select("svg").attr("height")], mapData);         
+        //create projection for map and pass that to the path generator
+        var projection = d3
+            .geoAlbers()
+            .rotate([0, 62, 0]);
 
         var path = d3.geoPath(projection);
 
-    svg
-        .append("g")
-        .selectAll("path")
-        .data(mapData.features)
-        .enter()
-        .append("path")
-        .attr("d", path)
-        .attr("class", "county")
-        .attr("id", function (d) {
-            return d.properties.county;
-        })
-        .attr("fill", "#000")
-        .style("stroke", "#d3d3d3")
-        .attr("transform", "translate(25, 0)");
 
-    // make initial annotation estimations and start annotations tool
-    var annotations = mapData.features.map(function (el) {
+        d3.json("/static/data/ncmap.json", function (error, mapData) {
+            if (error) throw error;
 
-        return {
-            note: { label: el.properties.county, orientation: "topBottom"},
-            x: path.centroid(el.geometry)[0],
-            y: path.centroid(el.geometry)[1] - 10
-        }
-    });
+            var projection = d3
+                .geoAlbers()
+                .rotate([0, 62, 0])
+                .fitSize([d3.select("svg").attr("width")-50, d3.select("svg").attr("height")], mapData);
 
-    window.makeAnnotations = d3.annotation()
-        .type(d3.annotationLabel)
-        .annotations(annotations)
-        // .disable(["connector", "subject"])
-        .editMode(true);
+            var path = d3.geoPath(projection);
 
-    svg.append("g")
-        .attr("class", "annotation-group")
-        .attr("transform", "translate(25, 0)")
-        .call(makeAnnotations);
+            svg
+                .append("g")
+                .selectAll("path")
+                .data(mapData.features)
+                .enter()
+                .append("path")
+                .attr("d", path)
+                .attr("class", "CountyName")
+                .attr("id", function (d) {
+                    return d.properties.CountyName;
+                })
+                .attr("fill", "#fff")
+                .style("stroke", "#d3d3d3")
+                .attr("transform", "translate(25, 0)");
 
-    document.getElementById("editMode").addEventListener("click", function () {
-        makeAnnotations.editMode(!makeAnnotations.editMode()).update();
-    });
-    console.log("Done")
-});
 
+
+
+            // make initial annotation estimations and start annotations tool
+            var annotations = mapData.features.map(function (el) {
+
+                return {
+                    note: { label: el.properties.CountyName, orientation: "topBottom"},
+                    x: path.centroid(el.geometry)[0],
+                    y: path.centroid(el.geometry)[1] - 10
+                }
+            });
+
+            window.makeAnnotations = d3.annotation()
+                .type(d3.annotationLabel)
+                .annotations(annotations)
+                // .disable(["connector", "subject"])
+                .editMode(false);
+
+            svg.append("g")
+                .attr("class", "annotation-group")
+                .attr("transform", "translate(25, 0)")
+                .call(makeAnnotations);
+
+            document.getElementById("editMode").addEventListener("click", function () {
+                makeAnnotations.editMode(!makeAnnotations.editMode()).update();
+            });
+        });
