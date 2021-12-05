5ba53ae0a55902feab87fd4af7992a6682e8a365
juliasulstarova <87027062+juliasulstarova@users.noreply.github.com>
2021-12-05 17:33:44 -0500

    Update life_expactancy.sql

--- life_expactancy.sql
+++ life_expactancy.sql
@@ -1,10 +1,9 @@
 CREATE TABLE life_expectancy(
   county_id INTEGER NOT NULL PRIMARY KEY REFERENCES county(id),
   life_exp_years INTEGER NOT NULL,
-  poverty_rate FLOAT NOT NULL, -- % of people living in poverty
-  unemployement_rate FLOAT NOT NULL, -- % of people unemployed
-  food_insecurity FLOAT NOT NULL, -- % of people suffering of food insecurity
-  violent_crime FLOAT NOT NULL -- % of people affected by violent crimes
+  poverty_rate FLOAT NOT NULL,
+  unemployement_rate FLOAT NOT NULL, 
+  violent_crime FLOAT NOT NULL
 );
 
 INSERT INTO life_expectancy VALUES (1, 78, 4.2, 33.8, 23.1, 45.1);
